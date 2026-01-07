6bffd706e4ec782061394c7245c22a8755b4051d
Fábio Santos <100423829+Gwin14@users.noreply.github.com>
2026-01-07 15:47:35 -0300

    Revert "feat: modo pwa offline"
    This reverts commit bfb5fb1cb9530e9f251e18e077fa4c1f7e65d295.

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { registerSW } from "virtual:pwa-register";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-registerSW({
-  onNeedRefresh() {
-    console.log("Nova versão disponível");
-  },
-  onOfflineReady() {
-    console.log("App pronto para uso offline");
-  },
-});
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
+  </StrictMode>,
+)
