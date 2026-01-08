111f49cc4874a908768f8e5648fc1eb0b32e3110
Fábio Santos <100423829+Gwin14@users.noreply.github.com>
2026-01-08 09:41:13 -0300

    feat: mais ícones de marcas

--- src/utils/cameraBrandLogos.js
+++ src/utils/cameraBrandLogos.js
@@ -10,67 +10,35 @@ import pentax from "../assets/brandIcons/pentax.png";
 import hasselblad from "../assets/brandIcons/hasselblad.png";
 import dji from "../assets/brandIcons/dji.png";
 import gopro from "../assets/brandIcons/gopro.png";
+import samsung from "../assets/brandIcons/samsung.png";
+import xiaomi from "../assets/brandIcons/xiaomi.png";
+import huawei from "../assets/brandIcons/huawei.png";
+import google from "../assets/brandIcons/google.png";
+import oneplus from "../assets/brandIcons/oneplus.png";
+import oppo from "../assets/brandIcons/oppo.png";
 
 const BRANDS = [
-  {
-    name: "Apple",
-    match: ["apple", "iphone"],
-    logo: apple,
-  },
-  {
-    name: "Canon",
-    match: ["canon"],
-    logo: canon,
-  },
-  {
-    name: "Nikon",
-    match: ["nikon"],
-    logo: nikon,
-  },
-  {
-    name: "Sony",
-    match: ["sony"],
-    logo: sony,
-  },
-  {
-    name: "Fujifilm",
-    match: ["fujifilm", "fuji"],
-    logo: fujifilm,
-  },
-  {
-    name: "Leica",
-    match: ["leica"],
-    logo: leica,
-  },
-  {
-    name: "Panasonic",
-    match: ["panasonic", "lumix"],
-    logo: panasonic,
-  },
+  { name: "Apple", match: ["apple", "iphone"], logo: apple },
+  { name: "Canon", match: ["canon"], logo: canon },
+  { name: "Nikon", match: ["nikon"], logo: nikon },
+  { name: "Sony", match: ["sony"], logo: sony },
+  { name: "Fujifilm", match: ["fujifilm", "fuji"], logo: fujifilm },
+  { name: "Leica", match: ["leica"], logo: leica },
+  { name: "Panasonic", match: ["panasonic", "lumix"], logo: panasonic },
+  { name: "Pentax", match: ["pentax", "ricoh"], logo: pentax },
+  { name: "Hasselblad", match: ["hasselblad"], logo: hasselblad },
+  { name: "DJI", match: ["dji"], logo: dji },
+  { name: "GoPro", match: ["gopro"], logo: gopro },
+  { name: "Samsung", match: ["samsung"], logo: samsung },
+  { name: "Xiaomi", match: ["xiaomi", "redmi", "poco"], logo: xiaomi },
+  { name: "Huawei", match: ["huawei", "honor"], logo: huawei },
+  { name: "Google", match: ["pixel", "google"], logo: google },
+  { name: "OnePlus", match: ["oneplus"], logo: oneplus },
+  { name: "Oppo", match: ["oppo", "realme"], logo: oppo },
   {
     name: "Olympus",
     match: ["olympus", "om system", "omsystem"],
     logo: olympus,
-  },
-  {
-    name: "Pentax",
-    match: ["pentax", "ricoh"],
-    logo: pentax,
-  },
-  {
-    name: "Hasselblad",
-    match: ["hasselblad"],
-    logo: hasselblad,
-  },
-  {
-    name: "DJI",
-    match: ["dji"],
-    logo: dji,
-  },
-  {
-    name: "GoPro",
-    match: ["gopro"],
-    logo: gopro,
   },
 ];
 
@@ -89,8 +57,5 @@ export function getCameraBrandLogo(make) {
     return { name: null, logo: null };
   }
 
-  return {
-    name: brand.name,
-    logo: brand.logo,
-  };
+  return { name: brand.name, logo: brand.logo };
 }
